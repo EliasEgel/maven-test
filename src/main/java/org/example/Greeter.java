77e02e6bc0e0c6b761777dfb8c528d300c77f9b9
Elias.Egel <elias.egelrid@gmail.com>
2025-03-16 08:58:17 +0100

    added Spring Boot functionality

--- src/main/java/org/example/Greeter.java
+++ src/main/java/org/example/Greeter.java
@@ -1,15 +1,23 @@
 package org.example;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) throws IOException {
 
+@SpringBootApplication
+public class Greeter implements CommandLineRunner {
+    public static void main(String[] args){
 
+        SpringApplication.run(Greeter.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
 
         System.out.print("When does your course start (yyyy-mm-dd) ? : ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
